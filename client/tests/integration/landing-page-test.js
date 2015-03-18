f0611f059248698af24abb328643a09dba92d71a
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2015-03-17 23:55:17 -0400

    Get a basic test and site title going.

---
+++ client/tests/integration/landing-page-test.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Integration - Landing Page', {
+  beforeEach: function() {
+    App = startApp();
+  },
+  afterEach: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should include the name United States of Baseball', function(assert) {
+  visit('/').then(function() {
+    assert.equal(find('h1#united_states_of_baseball').text(), 'United States of Baseball');
+  });
+});
