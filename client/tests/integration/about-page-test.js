ef322c2ef43dd5fc01f74008650dfc5f3939d6e2
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2015-03-21 23:29:34 -0400

    Comment out these example tests
    for functionality that I'm about to remove.

--- client/tests/integration/about-page-test.js
+++ client/tests/integration/about-page-test.js
@@ -1,30 +1,30 @@
-import Ember from 'ember';
-import { module, test } from 'qunit';
-import startApp from '../helpers/start-app';
-
-var App;
-
-module('Integration - About Page', {
-  beforeEach: function() {
-    App = startApp();
-  },
-  afterEach: function() {
-    Ember.run(App, 'destroy');
-  }
-});
-
-// test('Should navigate to the About page', function() {
-//   visit('/').then(function(assert) {
-//     click("a:contains('About')").then(function(assert) {
-//       assert.equal(find('h2').text(), 'About');
-//     });
-//   });
+// import Ember from 'ember';
+// import { module, test } from 'qunit';
+// import startApp from '../helpers/start-app';
+//
+// var App;
+//
+// module('Integration - About Page', {
+//   beforeEach: function() {
+//     App = startApp();
+//   },
+//   afterEach: function() {
+//     Ember.run(App, 'destroy');
+//   }
 // });
-
-// test('Should allow navigating back to root from another page', function(assert) {
-//   visit('/about').then(function() {
-//     click("a:contains('Home')").then(function(assert) {
-//       assert.notEqual(find('h2').text(), 'About');
-//     });
-//   });
-// });
+//
+// // test('Should navigate to the About page', function() {
+// //   visit('/').then(function(assert) {
+// //     click("a:contains('About')").then(function(assert) {
+// //       assert.equal(find('h2').text(), 'About');
+// //     });
+// //   });
+// // });
+//
+// // test('Should allow navigating back to root from another page', function(assert) {
+// //   visit('/about').then(function() {
+// //     click("a:contains('Home')").then(function(assert) {
+// //       assert.notEqual(find('h2').text(), 'About');
+// //     });
+// //   });
+// // });
