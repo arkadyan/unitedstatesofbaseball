e5a4795f15d0622b6ba202a4d3c89e3a71381b33
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2015-03-22 18:40:08 -0400

    Call it a state_code

--- client/app/router.js
+++ client/app/router.js
@@ -7,15 +7,8 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('teams', { path: '/' }, function() {
-    this.route('show', { path: ':team_id' });
-    // this.resource('team', { path: '/:state_code' });
+    this.route('show', { path: ':state_code' });
   });
 });
-
-// Route.TeamRoute = Ember.Route.extend({
-//   model: function() {
-//     return this.store.find('team', { code: params.state_code });
-//   }
-// });
 
 export default Router;
